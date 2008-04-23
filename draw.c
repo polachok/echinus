015ca6425417258fb2661a154b9790dff859229a
Alexander "polachok" Polakov <polachok@gmail.com>
2008-04-23 21:13:36 +0400

    move drawing to draw.c

---
+++ draw.c
@@ -0,0 +1,160 @@
+void
+drawtext(const char *text, unsigned long col[ColLast], Bool center) {
+    int x, y, w, h;
+    char buf[256];
+    unsigned int len, olen;
+    XRectangle r = { dc.x, 0, dc.w, dc.h };
+
+    XSetForeground(dpy, dc.gc, col[ColBG]);
+    XFillRectangles(dpy, dc.drawable, dc.gc, &r, 1);
+    if(!text)
+            return;
+    olen = len = strlen(text);
+    w = 0;
+    fprintf(stderr, "%d>>>w = %d<<<",__LINE__, w);
+    fprintf(stderr, "%d>>>w = %d len=%d<<<",__LINE__, w, len);
+    if(len >= sizeof buf)
+            len = sizeof buf - 1;
+    memcpy(buf, text, len);
+    buf[len] = 0;
+    fprintf(stderr, "%d>>>w = %d len=%d<<<",__LINE__, w, len);
+    h = dc.h;
+    y = dc.h-dc.font.height/2+1;
+    x = dc.x+h/2;
+    fprintf(stderr, "%d>>>w = %d dc.w = %d<<<",__LINE__, w, dc.w);
+    /* shorten text if necessary */
+    while(len && (w = textnw(buf, len)) > dc.w){
+        fprintf(stderr, "%d w = %d dc.w = %d\n",__LINE__, w, dc.w);
+            buf[--len] = 0;
+    }
+    fprintf(stderr, "%d>>>w = %d<<<",__LINE__, w);
+    if(len < olen) {
+            if(len > 1)
+                    buf[len - 1] = '.';
+            if(len > 2)
+                    buf[len - 2] = '.';
+            if(len > 3)
+                    buf[len - 3] = '.';
+    }
+    fprintf(stderr, "%d>>>w = %d<<<",__LINE__, w);
+    if(w > dc.w)
+            return; /* too long */
+    fprintf(stderr, "%d>>>w = %d<<<",__LINE__, w);
+    if(center)
+            x = dc.x + dc.w/2 - w/2;
+    else 
+            x = dc.x + h/2;
+
+    fprintf(stderr, "%d>>>w = %d<<<",__LINE__, w);
+    while(x <= 0)
+            x = dc.x++;
+    fprintf(stderr, "%d>>>w = %d<<<",__LINE__, w);
+    XftDrawStringUtf8(dc.xftdrawable, (col==dc.norm) ? dc.xftnorm : dc.xftsel,
+            dc.font.xftfont, x, y, (unsigned char*)buf, len);
+    fprintf(stderr, "x = %d %d w = %d\n", x,__LINE__, w);
+    dc.x = x + w;
+}
+
+Pixmap
+initpixmap(const char *file) {
+    Pixmap pmap;
+    pmap = XCreatePixmap(dpy, root, 20, 20, 1);
+    unsigned int pw, ph;
+    if(BitmapSuccess == XReadBitmapFile(dpy, root, file, &pw, &ph, &pmap, &px, &py))
+        return pmap;
+    else
+        eprint("echinus: cannot load button pixmaps, check your ~/.echinusrc\n");
+    return 0;
+}
+
+void
+initbuttons() {
+    XSetForeground(dpy, dc.gc, dc.norm[ColButton]);
+    XSetBackground(dpy, dc.gc, dc.norm[ColBG]);
+    bleft.pm = initpixmap(getresource("button.left.pixmap", BLEFTPIXMAP));
+    bright.pm = initpixmap(getresource("button.right.pixmap", BRIGHTPIXMAP));
+    bcenter.pm = initpixmap(getresource("button.center.pixmap", BCENTERPIXMAP));
+    bleft.action = iconifyit;
+    bright.action = killclient;
+    bcenter.action = togglemax;
+}
+
+void
+drawbuttons(Client *c) {
+    XSetForeground(dpy, dc.gc, (c == sel) ? dc.sel[ColButton] : dc.norm[ColButton]);
+    XSetBackground(dpy, dc.gc, (c == sel) ? dc.sel[ColBG] : dc.norm[ColBG]);
+    XCopyPlane(dpy, bright.pm, dc.drawable, dc.gc, px*2, py*2, c->th, c->th*2, c->tw-c->th, 0, 1);
+    XCopyPlane(dpy, bleft.pm, dc.drawable, dc.gc, px*2, py*2, c->th, c->th*2, c->tw-3*c->th, 0, 1);
+    XCopyPlane(dpy, bcenter.pm, dc.drawable, dc.gc, px*2, py*2, c->th, c->th*2, c->tw-2*c->th, 0, 1);
+}
+
+void
+drawclient(Client *c) {
+    unsigned int i;
+    unsigned int opacity;
+    if(NOTITLES)
+        return;
+    if(!isvisible(c))
+        return;
+    resizetitle(c);
+    XSetForeground(dpy, dc.gc, dc.norm[ColBG]);
+    XFillRectangle(dpy, c->title, dc.gc, 0, 0, c->tw, c->th);
+    dc.x = dc.y = 0;
+    dc.w = c->w;
+    dc.h = c->th;
+    for(i=0; i <= ntags; i++) {
+        if(seltags[i]){
+            drawtext(tags[i], c == sel ? dc.sel : dc.norm, False);
+            XSetForeground(dpy, dc.gc, c== sel ? dc.sel[ColBorder] : dc.norm[ColBorder]);
+            XDrawLine(dpy, dc.drawable, dc.gc, dc.x+dc.h/2-1, 0, dc.x+dc.h/2-1, dc.h);
+            dc.x+=dc.h/2;
+        }
+    }
+    drawtext(c->name, c == sel ? dc.sel : dc.norm, False);
+    if(c->tw>=6*c->th && dc.x <= c->tw-6*c->th)
+        drawbuttons(c);
+    XCopyArea(dpy, dc.drawable, c->title, dc.gc,
+			0, 0, c->tw, c->th+2*borderpx, 0, 0);
+    if (c==sel)
+      opacity = OPAQUE;
+    else
+      opacity = (unsigned int) (uf_opacity * OPAQUE);
+    setopacity(c, opacity);
+    XMapWindow(dpy, c->title);
+}
+
+void
+drawfloating() {
+    Client *c;
+    for(c = clients; c; c=c->next){
+        if(c->isfloating){
+            c->hastitle = c->hadtitle;
+            drawclient(c);
+        }
+    }
+}
+
+static void
+initfont(const char *fontstr) {
+    dc.font.xftfont = XftFontOpenXlfd(dpy,screen,fontstr);
+    if(!dc.font.xftfont)
+         dc.font.xftfont = XftFontOpenName(dpy,screen,fontstr);
+    if(!dc.font.xftfont)
+         eprint("error, cannot load font: '%s'\n", fontstr);
+    dc.font.extents = emallocz(sizeof(XGlyphInfo));
+    XftTextExtentsUtf8(dpy,dc.font.xftfont,(unsigned char*)fontstr, strlen(fontstr), dc.font.extents);
+    dc.font.height = dc.font.extents->y+dc.font.extents->yOff;
+    dc.font.width = (dc.font.extents->width)/strlen(fontstr);
+}
+
+unsigned int
+textnw(const char *text, unsigned int len) {
+    XftTextExtentsUtf8(dpy,dc.font.xftfont,(unsigned char*)text, strlen(text), dc.font.extents);
+    fprintf(stderr, "width = %d len = %d", dc.font.extents->width, len);
+    return dc.font.extents->width;
+}
+
+unsigned int
+textw(const char *text) {
+    return textnw(text, strlen(text));
+}
--- echinus.c
+++ echinus.c
@@ -278,6 +278,7 @@ int dectiled = 0;
 #include "config.h"
 #include "ewmh.c"
 #include "parse.c"
+#include "draw.c"
 
 void (*handler[LASTEvent]) (XEvent *) = {
 	[ButtonPress] = buttonpress,
@@ -718,120 +719,6 @@ detachstack(Client *c) {
     *tc = c->snext;
 }
 
-void
-drawtext(const char *text, unsigned long col[ColLast], Bool center) {
-    int x, y, w, h;
-    static char buf[256];
-    unsigned int len, olen;
-    XRectangle r = { dc.x, 0, dc.w, dc.h };
-
-    XSetForeground(dpy, dc.gc, col[ColBG]);
-    XFillRectangles(dpy, dc.drawable, dc.gc, &r, 1);
-    if(!text)
-            return;
-    w = 0;
-    olen = len = strlen(text);
-    if(len >= sizeof buf)
-            len = sizeof buf - 1;
-    memcpy(buf, text, len);
-    buf[len] = 0;
-    h = dc.h;
-    y = dc.h-dc.font.height/2+1;
-    x = dc.x+h/2;
-            /* shorten text if necessary */
-    while(len && (w = textnw(buf, len)) > dc.w)
-            buf[--len] = 0;
-    if(len < olen) {
-            if(len > 1)
-                    buf[len - 1] = '.';
-            if(len > 2)
-                    buf[len - 2] = '.';
-            if(len > 3)
-                    buf[len - 3] = '.';
-    }
-    if(w > dc.w)
-            return; /* too long */
-    if(center)
-            x = dc.x + dc.w/2 - w/2;
-    else 
-            x = dc.x + h/2;
-    while(x <= 0)
-            x = dc.x++;
-    XftDrawStringUtf8(dc.xftdrawable, (col==dc.norm) ? dc.xftnorm : dc.xftsel,
-            dc.font.xftfont, x, y, (unsigned char*)buf, len);
-    dc.x = x + w;
-}
-
-Pixmap
-initpixmap(const char *file) {
-    Pixmap pmap;
-    pmap = XCreatePixmap(dpy, root, 20, 20, 1);
-    unsigned int pw, ph;
-    if(BitmapSuccess == XReadBitmapFile(dpy, root, file, &pw, &ph, &pmap, &px, &py))
-        return pmap;
-    else
-        eprint("echinus: cannot load button pixmaps, check your ~/.echinusrc\n");
-    return 0;
-}
-
-void
-initbuttons() {
-    XSetForeground(dpy, dc.gc, dc.norm[ColButton]);
-    XSetBackground(dpy, dc.gc, dc.norm[ColBG]);
-    bleft.pm = initpixmap(getresource("button.left.pixmap", BLEFTPIXMAP));
-    bright.pm = initpixmap(getresource("button.right.pixmap", BRIGHTPIXMAP));
-    bcenter.pm = initpixmap(getresource("button.center.pixmap", BCENTERPIXMAP));
-    bleft.action = iconifyit;
-    bright.action = killclient;
-    bcenter.action = togglemax;
-}
-
-void
-drawbuttons(Client *c) {
-    XSetForeground(dpy, dc.gc, (c == sel) ? dc.sel[ColButton] : dc.norm[ColButton]);
-    XSetBackground(dpy, dc.gc, (c == sel) ? dc.sel[ColBG] : dc.norm[ColBG]);
-    XCopyPlane(dpy, bright.pm, dc.drawable, dc.gc, px*2, py*2, c->th, c->th*2, c->tw-c->th, 0, 1);
-    XCopyPlane(dpy, bleft.pm, dc.drawable, dc.gc, px*2, py*2, c->th, c->th*2, c->tw-3*c->th, 0, 1);
-    XCopyPlane(dpy, bcenter.pm, dc.drawable, dc.gc, px*2, py*2, c->th, c->th*2, c->tw-2*c->th, 0, 1);
-}
-
-void
-drawclient(Client *c) {
-    unsigned int opacity;
-    if(NOTITLES)
-        return;
-    if(!isvisible(c))
-        return;
-    resizetitle(c);
-    XSetForeground(dpy, dc.gc, dc.norm[ColBG]);
-    XFillRectangle(dpy, c->title, dc.gc, 0, 0, c->tw, c->th);
-    dc.x = dc.y = 0;
-    dc.w = c->w+borderpx;
-    dc.h = c->th;
-    drawtext(c->name, c == sel ? dc.sel : dc.norm, False);
-    if(c->tw>=6*c->th && dc.x <= c->tw-6*c->th)
-        drawbuttons(c);
-    XCopyArea(dpy, dc.drawable, c->title, dc.gc,
-			0, 0, c->tw, c->th+2*borderpx, 0, 0);
-    if (c==sel)
-      opacity = OPAQUE;
-    else
-      opacity = (unsigned int) (uf_opacity * OPAQUE);
-    setopacity(c, opacity);
-    XMapWindow(dpy, c->title);
-}
-
-void
-drawfloating() {
-    Client *c;
-    for(c = clients; c; c=c->next){
-        if(c->isfloating){
-            c->hastitle = c->hadtitle;
-            drawclient(c);
-        }
-    }
-}
-
 void *
 emallocz(unsigned int size) {
     void *res = calloc(1, size);
@@ -876,19 +763,6 @@ expose(XEvent *e) {
     Client *c;
     if((c=gettitle(ev->window)))
         drawclient(c);
-}
-
-static void
-initfont(const char *fontstr) {
-    dc.font.xftfont = XftFontOpenXlfd(dpy,screen,fontstr);
-    if(!dc.font.xftfont)
-         dc.font.xftfont = XftFontOpenName(dpy,screen,fontstr);
-    if(!dc.font.xftfont)
-         eprint("error, cannot load font: '%s'\n", fontstr);
-    dc.font.extents = emallocz(sizeof(XGlyphInfo));
-    XftTextExtentsUtf8(dpy,dc.font.xftfont,(unsigned char*)fontstr, strlen(fontstr), dc.font.extents);
-    dc.font.height = dc.font.extents->y+dc.font.extents->yOff;
-    dc.font.width = (dc.font.extents->width)/strlen(fontstr);
 }
 
 void
@@ -2031,19 +1905,6 @@ tile(void) {
                 }
         }
         drawfloating();
-}
-
-unsigned int
-textnw(const char *text, unsigned int len) {
-    XftTextExtentsUtf8(dpy,dc.font.xftfont,(unsigned char*)text, strlen(text), dc.font.extents);
-    if(dc.font.extents->width/len > dc.font.width)
-         dc.font.width = dc.font.extents->width/len;
-    return dc.font.extents->width;
-}
-
-unsigned int
-textw(const char *text) {
-    return textnw(text, strlen(text)) + 2*dc.font.width;
 }
 
 void
