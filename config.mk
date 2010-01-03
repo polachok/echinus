d14200499291f1c11f1020a0a64757081711f8f8
Alexander Polakov <polachok@gmail.com>
2010-01-03 19:36:22 +0300

    Add more warnings to config.mk (for a moment).
    Submitted-by: Guillermo A. Amaral B <gamaral@amaral.com.mx>

--- config.mk
+++ config.mk
@@ -18,8 +18,13 @@ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 `pkg-config --libs xft`
 # flags
 CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\" -DSYSCONFPATH=\"${PREFIX}/${CONF}\"
 LDFLAGS = -s ${LIBS}
-CFLAGS = -g3 -ggdb3 -std=c99 -pedantic -Wall -O0 ${INCS} -DVERSION=\"${VERSION}\" -DSYSCONFPATH=\"${PREFIX}/${CONF}\"
+CFLAGS = -g3 -ggdb3 -std=c99 -pedantic -O0 ${INCS} -DVERSION=\"${VERSION}\" -DSYSCONFPATH=\"${PREFIX}/${CONF}\"
 LDFLAGS = -g3 -ggdb3 ${LIBS}
+
+# DEBUG: Show warnings (if any). Comment out to disable.
+CFLAGS += -Wall -W -Wcast-qual -Wshadow -Wwrite-strings
+#CFLAGS += -Werror        # Treat warnings as errors.
+#CFLAGS += -save-temps    # Keep precompiler output (great for debugging).
 
 # XRandr (multihead support). Comment out to disable.
 CFLAGS += -DXRANDR=1
