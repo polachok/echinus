7b8377b0096cb46d722b4e22a68e1503dccb5927
Alexander Polakov <polachok@gmail.com>
2011-10-17 14:58:36 +0400

    ewmh: kill extra parenthesis

--- ewmh.c
+++ ewmh.c
@@ -222,7 +222,7 @@ ewmh_process_state_atom(Client *c, Atom state, int set) {
 	CARD32 data[2];
 
 	data[1] = None;
-	if ((state == atom[WindowStateFs])) {
+	if (state == atom[WindowStateFs]) {
 		focus(c);
 		if (set && !c->ismax) {
 			c->wasfloating = c->isfloating;
